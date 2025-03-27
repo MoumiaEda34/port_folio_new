be631e4c200e8ad4902fdb1c246d2860f12addfb
MoumiaEda34 <moumita.ahamed7860@gmail.com>
2025-03-27 15:24:08 +0530

    Fix build issues

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  distDir: "build", // Move `distDir` inside nextConfig
+  distDir: '.next',
 };
 
 export default nextConfig;
